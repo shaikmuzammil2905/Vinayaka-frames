facc51aaa401dd7af47d80f53d27228232bb414e
codtech1208 <harishneela83@gmail.com>
2026-09-18 17:14:00 +0530

    feat: restore Home route for root path

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { WishlistProvider } from './context/WishlistContext';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { BottomNavigation } from './components/BottomNavigation';
+import { Home } from './pages/Home';
 import { SearchPage } from './pages/SearchPage';
 import { ProductListing } from './pages/ProductListing';
 import { ProductDetail } from './pages/ProductDetail';
@@ -23,7 +24,7 @@ function App() {
             <Header />
             <main className="flex-1 bg-gray-50">
               <Routes>
-                <Route path="/" element={<ProductListing />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/search" element={<SearchPage />} />
                 <Route path="/deals" element={<ProductListing isDealsPage={true} />} />
                 <Route path="/categories" element={<ProductListing />} />
@@ -35,7 +36,7 @@ function App() {
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/login" element={<Login />} />
                 {/* Fallback */}
-                <Route path="*" element={<ProductListing />} />
+                <Route path="*" element={<Home />} />
               </Routes>
             </main>
             <Footer />
